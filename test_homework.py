fdec40a585a537563d5b3ac277f25db5b9dde365
Victoria Kireitseva <victoria.kireitseva@gmail.com>
2025-02-17 13:10:28 +0300

    убрала внешние скобки из (list(set(l))) раскомментировала .idea

--- test_homework.py
+++ test_homework.py
@@ -59,7 +59,7 @@ def test_unique_elements():
     Удалите из списка все повторяющиеся элементы
     """
     l = {1, 2, 3, 4, 5, 5, 5, 6, 7, 8, 8, 9, 10, 10}
-    l = (list(set(l)))
+    l = list(set(l))
 
     assert isinstance(l, list)
     assert len(l) == 10
